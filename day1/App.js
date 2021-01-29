c34d4e05f380bff8b93076adb344c3deee6cd9fe
pip-pipo <codemi692@gmail>
2021-01-28 14:00:01 -1100

    :m:Es7:rocket:Es8:fire:

--- day1/App.js
+++ day1/App.js
@@ -1,3 +1,55 @@
-const data = [{namek:'hello'}]
 
-console.log(data[0]?.name)
+//  Es7 and Es8 Features
+
+//  padStart() and padEnd() return string
+const string = "string"
+
+console.log(string.padStart(9, '.'));
+console.log(string.padEnd(9, '.'));
+
+
+
+// Object.values()  return array of any
+const ob1 = { name: 'morsalin', roll: 4, age: 17 }
+const obj = { imran: ['naem', 'kdfm', 'dkfjk'] }
+
+console.log(Object.values(obj));
+console.log(Object.values(ob1));
+
+
+
+// Object.entries() return array of any
+const objData = {
+    user1:{
+        nmae:'morsalin',
+        roll:3,
+        age:16,
+        hobby:'programming',
+    },
+    user2:{
+        nmae:'rony',
+        roll:6,
+        age:18,
+        hobby:'football',
+    },
+    user3:{
+        nmae:'rasle',
+        roll:4,
+        age:28,
+        hobby:'reading book',
+    },
+}
+
+
+console.log(Object.entries(objData));
+
+
+// .toExponential()
+console.log(Number.parseFloat(3).toExponential(3));
+
+
+// Exponential
+// ** means pow
+// will show same output
+console.log(Math.pow(5,2));
+console.log(5 ** 2);
